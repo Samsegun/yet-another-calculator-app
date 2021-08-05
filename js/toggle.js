0872ded1987e8812561566de5d043863876c96d1
Samsegun <oyebadesamsegun@gmail.com>
2021-08-04 20:29:50 -1200

    toggle js refactored, possible refactor at a later date

--- js/toggle.js
+++ js/toggle.js
@@ -1,72 +1,60 @@
 // event listener for toggleswitch
 toggleBox.forEach((box) => {
   box.addEventListener("click", (e) => {
-    if (box.classList.contains("check-1_box")) {
-      addRemove(LIGHT, MAGENTA, DARKGRAY);
-      wrapper.classList.remove("theme-2-bg", "theme-3-bg");
-      wrapper.classList.add("theme-1-bg");
-      contentWrapper.classList.remove("theme-2-color", "theme-3-color");
-      contentWrapper.classList.add("theme-1-color");
-      slide.classList.remove("theme-2-slide", "theme-3-slide");
-      slide.classList.add("theme-1-slide");
-      input.classList.remove("theme-2-screen", "theme-3-screen");
-      input.classList.add("theme-1-screen");
-      keysWrapper.classList.remove("theme-2-kwrapper", "theme-3-kwrapper");
-      keysWrapper.classList.add("theme-1-kwrapper");
+    if (box.classList.contains(CHECKBOX1)) {
+      addRemoveFromClass(slider, LIGHT, MAGENTA, DARKGRAY);
+      addRemoveFromClass(wrapper, THEME2BG, THEME3BG, THEME1BG);
+      addRemoveFromClass(contentWrapper, THEME2COLOR, THEME3COLOR, THEME1COLOR);
+      addRemoveFromClass(slide, THEME2SLIDE, THEME3SLIDE, THEME1SLIDE);
+      addRemoveFromClass(input, THEME2SCREEN, THEME3SCREEN, THEME1SCREEN);
+      addRemoveFromClass(
+        keysWrapper,
+        THEME2KWRAPPER,
+        THEME3KWRAPPER,
+        THEME1KWRAPPER,
+      );
       buttons.forEach((button) => {
-        button.classList.remove("theme-2-button", "theme-3-button");
-        button.classList.add("theme-1-button");
+        addRemoveFromClass(button, THEME2BUTTON, THEME3BUTTON, THEME1BUTTON);
       });
-      del.classList.remove("theme-2-same", "theme-3-same");
-      del.classList.add("theme-1-same");
-      reset.classList.remove("theme-2-same", "theme-3-same");
-      reset.classList.add("theme-1-same");
-      equals.classList.remove("theme-2-equals", "theme-3-equals");
-      equals.classList.add("theme-1-equals");
-    } else if (box.classList.contains("check-2_box")) {
-      addRemove(DARKGRAY, MAGENTA, LIGHT);
-      wrapper.classList.remove("theme-1-bg", "theme-3-bg");
-      wrapper.classList.add("theme-2-bg");
-      contentWrapper.classList.remove("theme-1-color", "theme-3-color");
-      contentWrapper.classList.add("theme-2-color");
-      slide.classList.remove("theme-1-slide", "theme-3-slide");
-      slide.classList.add("theme-2-slide");
-      input.classList.remove("theme-1-screen", "theme-3-screen");
-      input.classList.add("theme-2-screen");
-      keysWrapper.classList.remove("theme-1-kwrapper", "theme-3-kwrapper");
-      keysWrapper.classList.add("theme-2-kwrapper");
+      addRemoveFromClass(del, THEME2SAME, THEME3SAME, THEME1SAME);
+      addRemoveFromClass(reset, THEME2SAME, THEME3SAME, THEME1SAME);
+      addRemoveFromClass(equals, THEME2EQUALS, THEME3EQUALS, THEME1EQUALS);
+    } else if (box.classList.contains(CHECKBOX2)) {
+      addRemoveFromClass(slider, MAGENTA, DARKGRAY, LIGHT);
+      addRemoveFromClass(wrapper, THEME1BG, THEME3BG, THEME2BG);
+      addRemoveFromClass(contentWrapper, THEME1COLOR, THEME3COLOR, THEME2COLOR);
+      addRemoveFromClass(slide, THEME1SLIDE, THEME3SLIDE, THEME2SLIDE);
+      addRemoveFromClass(input, THEME1SCREEN, THEME3SCREEN, THEME2SCREEN);
+      addRemoveFromClass(
+        keysWrapper,
+        THEME1KWRAPPER,
+        THEME3KWRAPPER,
+        THEME2KWRAPPER,
+      );
       buttons.forEach((button) => {
-        button.classList.remove("theme-1-button", "theme-3-button");
-        button.classList.add("theme-2-button");
+        addRemoveFromClass(button, THEME1BUTTON, THEME3BUTTON, THEME2BUTTON);
       });
-      del.classList.remove("theme-1-same", "theme-3-same");
-      del.classList.add("theme-2-same");
-      reset.classList.remove("theme-1-same", "theme-3-same");
-      reset.classList.add("theme-2-same");
-      equals.classList.remove("theme-1-equals", "theme-3-equals");
-      equals.classList.add("theme-2-equals");
+      addRemoveFromClass(del, THEME1SAME, THEME3SAME, THEME2SAME);
+      addRemoveFromClass(reset, THEME1SAME, THEME3SAME, THEME2SAME);
+      addRemoveFromClass(equals, THEME1EQUALS, THEME3EQUALS, THEME2EQUALS);
     } else {
-      addRemove(DARKGRAY, LIGHT, MAGENTA);
-      wrapper.classList.remove("theme-1-bg", "theme-2-bg");
-      wrapper.classList.add("theme-3-bg");
-      contentWrapper.classList.remove("theme-1-color", "theme-2-color");
-      contentWrapper.classList.add("theme-3-color");
-      slide.classList.remove("theme-1-slide", "theme-2-slide");
-      slide.classList.add("theme-3-slide");
-      input.classList.remove("theme-1-screen", "theme-2-screen");
-      input.classList.add("theme-3-screen");
-      keysWrapper.classList.remove("theme-1-kwrapper", "theme-2-kwrapper");
-      keysWrapper.classList.add("theme-3-kwrapper");
+      addRemoveFromClass(slider, LIGHT, DARKGRAY, MAGENTA);
+      addRemoveFromClass(wrapper, THEME1BG, THEME2BG, THEME3BG);
+      addRemoveFromClass(contentWrapper, THEME1COLOR, THEME2COLOR, THEME3COLOR);
+      addRemoveFromClass(slide, THEME1SLIDE, THEME2SLIDE, THEME3SLIDE);
+      addRemoveFromClass(input, THEME1SCREEN, THEME2SCREEN, THEME3SCREEN);
+      addRemoveFromClass(
+        keysWrapper,
+        THEME1KWRAPPER,
+        THEME2KWRAPPER,
+        THEME3KWRAPPER,
+      );
       buttons.forEach((button) => {
-        button.classList.remove("theme-1-button", "theme-2-button");
-        button.classList.add("theme-3-button");
+        addRemoveFromClass(button, THEME1BUTTON, THEME2BUTTON, THEME3BUTTON);
       });
-      del.classList.remove("theme-1-same", "theme-2-same");
-      del.classList.add("theme-3-same");
-      reset.classList.remove("theme-1-same", "theme-2-same");
-      reset.classList.add("theme-3-same");
-      equals.classList.remove("theme-1-equals", "theme-2-equals");
-      equals.classList.add("theme-3-equals");
+      addRemoveFromClass(del, THEME1SAME, THEME2SAME, THEME3SAME);
+      addRemoveFromClass(reset, THEME1SAME, THEME2SAME, THEME3SAME);
+      addRemoveFromClass(equals, THEME1EQUALS, THEME2EQUALS, THEME3EQUALS);
     }
   });
 });
--- js/vendor.js
+++ js/vendor.js
@@ -16,15 +16,61 @@ const equals = document.getElementById("equals");
 const backDrop = document.querySelector(".backdrop");
 const notification = document.querySelector(".notification");
 
-// global identifiers
+// identifiers for chechbox
+const CHECKBOX1 = "check-1_box";
+const CHECKBOX2 = "check-2_box";
+const CHECKBOX3 = "check-3_box";
+
+// identifiers for slider
 const DARKGRAY = "dark-gray";
 const LIGHT = "light";
 const MAGENTA = "magenta";
 
-// function for toggling classes on slider
+// identifiers for wrapper
+const THEME1BG = "theme-1-bg";
+const THEME2BG = "theme-2-bg";
+const THEME3BG = "theme-3-bg";
+
+// identifiers for content wrapper
+const THEME1COLOR = "theme-1-color";
+const THEME2COLOR = "theme-2-color";
+const THEME3COLOR = "theme-3-color";
+
+// identifiers for slide
+const THEME1SLIDE = "theme-1-slide";
+const THEME2SLIDE = "theme-2-slide";
+const THEME3SLIDE = "theme-3-slide";
+
+// identifiers for input
+const THEME1SCREEN = "theme-1-screen";
+const THEME2SCREEN = "theme-2-screen";
+const THEME3SCREEN = "theme-3-screen";
+
+// identifiers for keys wrapper
+const THEME1KWRAPPER = "theme-1-kwrapper";
+const THEME2KWRAPPER = "theme-2-kwrapper";
+const THEME3KWRAPPER = "theme-3-kwrapper";
+
+// identifiers for buttons
+const THEME1BUTTON = "theme-1-button";
+const THEME2BUTTON = "theme-2-button";
+const THEME3BUTTON = "theme-3-button";
+
+// identifiers for delete and reset
+const THEME1SAME = "theme-1-same";
+const THEME2SAME = "theme-2-same";
+const THEME3SAME = "theme-3-same";
+
+// identifiers for equals
+const THEME1EQUALS = "theme-1-equals";
+const THEME2EQUALS = "theme-2-equals";
+const THEME3EQUALS = "theme-3-equals";
+
+// functions for toggling classes
+
 // 1st&2nd parameters are classes to be removed
 // last parameter is the class to be added
-function addRemove(class1, class2, class3) {
-  slider.classList.remove(class1, class2);
-  slider.classList.add(class3);
+function addRemoveFromClass(element, class1, class2, class3) {
+  element.classList.remove(class1, class2);
+  element.classList.add(class3);
 }
